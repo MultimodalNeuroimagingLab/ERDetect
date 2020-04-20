09e854c10a484c10e5b4fc4920f7d8db8250a037
Max <m.a.vandenboom84@gmail.com>
2020-04-20 13:35:40 -0500

    Docker debug

--- functions/load_bids.py
+++ functions/load_bids.py
@@ -197,6 +197,8 @@ def load_data_epochs(data_path, channels, onsets, epoch_start, epoch_end):
         data = np.empty((len(channels), len(onsets), size_time_s))
         print('test2')
         data[len(channels) - 1, len(onsets) - 1, 0] = np.nan
+        data[65, 397, :] = np.nan
+
         #data.fill(np.nan)
         print('test3')
 
