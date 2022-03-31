addf05e2fda1cf848999364724c4cd5504da1168
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2022-03-31 16:35:06 -0500

    Update quotation marks in help text

--- main_run.py
+++ main_run.py
@@ -72,21 +72,21 @@ parser.add_argument('output_dir',
                          'participant level analysis.\n\n')
 parser.add_argument('--participant_label',
                     help='The label(s) of the participant(s) that should be analyzed. The label corresponds\n'
-                         'to sub-<participant_label> from the BIDS spec (so it does not include "sub-").\n'
+                         'to sub-<participant_label> from the BIDS spec (so it does not include \'sub-\').\n'
                          'If this parameter is not provided all subjects will be analyzed. Multiple\n'
                          'participants can be specified with a space separated list.\n\n',
                     nargs="+")
 parser.add_argument('--subset_search_pattern',
                     help='The subset(s) of data that should be analyzed. The pattern should be part of a BIDS\n'
-                         'compliant folder name (e.g. "task-ccep_run-01"). If this parameter is not provided\n'
+                         'compliant folder name (e.g. \'task-ccep_run-01\'). If this parameter is not provided\n'
                          'all the found subset(s) will be analyzed. Multiple subsets can be specified with\n'
                          'a space separated list.\n\n',
                     nargs="+")
 parser.add_argument('--format_extension',
                     help='The data format(s) to include. The format(s) should be specified by their\n'
-                         'extension (e.g. ".edf"). If this parameter is not provided, then by default\n'
-                         'the European Data Format (''.edf''), BrainVision (''.vhdr'', ''.vmrk'', ''.eeg'')\n'
-                         'and MEF3 (''.mefd'') formats will be included. Multiple formats can be specified\n'
+                         'extension (e.g. \'.edf\'). If this parameter is not provided, then by default\n'
+                         'the European Data Format (\'.edf\'), BrainVision (\'.vhdr\', \'.vmrk\', \'.eeg\')\n'
+                         'and MEF3 (\'.mefd\') formats will be included. Multiple formats can be specified\n'
                          'with a space separated list.\n\n',
                     nargs="+")
 parser.add_argument('--config_filepath',
@@ -108,36 +108,36 @@ parser.add_argument('--preproc_prioritize_speed',
                     action='store_true')
 parser.add_argument('--high_pass',
                     help='Perform high-pass filtering (with a cut-off at 0.50Hz) before detection and visualization.\n'
-                         'Note: If a configuration file is provided, then this command-line argument will overrule\n'
-                         '      the high-pass setting in the configuration file\n\n',
+                         'Note: If a configuration file is provided, then this command-line argument will overrule the\n'
+                         '      high-pass setting in the configuration file\n\n',
                     action='store_true')
 parser.add_argument('--early_reref',
                     help='Perform early re-referencing before detection and visualization. The options are:\n'
-                         '      - CAR   = Common Average Re-refrencing (e.g. ''--early_reref CAR'')\n'
-                         'Note: If a configuration file is provided, then this command-line argument will overrule\n'
-                         '      the early re-referencing setting in the configuration file\n\n',
+                         '      - CAR   = Common Average Re-refrencing (e.g. \'--early_reref CAR\')\n'
+                         'Note: If a configuration file is provided, then this command-line argument will overrule the\n'
+                         '      early re-referencing setting in the configuration file\n\n',
                     nargs="?")
 parser.add_argument('--line_noise_removal',
                     help='Perform line-noise removal before detection and visualization. Can be either:\n'
-                         '      - ''tsv'' to lookup the line-noise frequency in the BIDS channels.tsv file\n'
-                         '        (e.g. ''--line_noise_removal tsv'')\n'
-                         '      - set to a specific line-noise frequency (e.g. ''--line_noise_removal 60'')\n'
-                         'Note: If a configuration file is provided, then this command-line argument will overrule\n'
-                         '      the line-noise removal setting in the configuration file\n\n',
+                         '      - \'tsv\' to lookup the line-noise frequency in the BIDS channels.tsv file\n'
+                         '        (e.g. \'--line_noise_removal tsv\')\n'
+                         '      - set to a specific line-noise frequency (e.g. \'--line_noise_removal 60\')\n'
+                         'Note: If a configuration file is provided, then this command-line argument will overrule the\n'
+                         '      line-noise removal setting in the configuration file\n\n',
                     nargs="?")
 parser.add_argument('--method',
                     help='The method that should be used to detect evoked responses. the options are:\n'
-                         '      - std_base   = The standard deviation of a baseline-epoch is used as a threshold (multiplied\n'
-                         '                     by a factor) to determine whether the average evoked deflection is strong\n'
-                         '                     enough. (e.g. ''--method std_base'')\n'
+                         '      - std_base   = The standard deviation of a baseline-epoch is used as a threshold'
+                         '                     (multiplied by a factor) to determine whether the average evoked deflection'
+                         '                     is strong enough. (e.g. \'--method std_base\')\n'
                          '      - cross-proj = Cross-projection of the trials is used to determine the inter-trial\n'
                          '                     similarity. A peak with a strong inter-trial similarity is\n'
-                         '                     considered an evoked response. (e.g. ''--method cross-proj'')\n'
+                         '                     considered an evoked response. (e.g. \'--method cross-proj\')\n'
                          '      - waveform   = Searches for the typical (20Hz oscillation) shape of the average response\n'
                          '                     to determine whether the peak that was found can be considered an evoked.\n'
-                         '                     response (e.g. ''--method waveform'')\n'
-                         'Note: If a configuration file is provided, then this command-line argument will overrule\n'
-                         '      the method setting in the configuration file\n\n',
+                         '                     response (e.g. \'--method waveform\')\n'
+                         'Note: If a configuration file is provided, then this command-line argument will overrule the\n'
+                         '      method setting in the configuration file\n\n',
                     nargs="?")
 parser.add_argument('-v', '--version',
                     action='version',
