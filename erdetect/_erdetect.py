5dad7cefc3c9fe9d65ae37ae244dc7861868ba29
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2022-09-13 17:56:08 -0500

    Renamed output images module

--- erdetect/_erdetect.py
+++ erdetect/_erdetect.py
@@ -6,7 +6,7 @@ import scipy.io as sio
 
 from core.config import write_config, get as cfg, get_config_dict, OUTPUT_IMAGE_SIZE, LOGGING_CAPTION_INDENT_LENGTH
 from core.detection import ieeg_detect_er
-from views.views import calc_sizes_and_fonts, calc_matrix_image_size, gen_amplitude_matrix, gen_latency_matrix
+from views.output_images import calc_sizes_and_fonts, calc_matrix_image_size, gen_amplitude_matrix, gen_latency_matrix
 from utils.bids import load_channel_info, load_event_info, load_ieeg_sidecar, load_data_epochs_averages, RerefStruct
 from utils.misc import print_progressbar, is_number, multi_line_list, create_figure
 from metric_callbacks import metric_cross_proj, metric_waveform
