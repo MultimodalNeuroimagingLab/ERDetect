7c8a31cf3ab7369584b6be80618c890b7d608f61
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2025-01-09 09:09:01 +0100

    Bump dependency versions (ieegPrep/PyMef for ARM64 compatibility; Python 3.9 and Numpy 2)

--- erdetect/__init__.py
+++ erdetect/__init__.py
@@ -4,8 +4,8 @@
 # ensure minimum version of python
 #
 import sys
-if sys.version_info < (3, 8, 0):
-    sys.exit("Python 3.8 or later is required.")
+if sys.version_info < (3, 9, 0):
+    sys.exit("Python 3.9 or later is required.")
 
 
 #
